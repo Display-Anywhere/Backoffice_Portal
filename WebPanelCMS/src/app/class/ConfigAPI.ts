bfccdb1aea86236f870be513c9903900a00d65be
Talwinder Jeet Singh <talwindergur@gmail.com>
2021-02-17 13:07:50 +0530

    merge with live

--- WebPanelCMS/src/app/class/ConfigAPI.ts
+++ WebPanelCMS/src/app/class/ConfigAPI.ts
@@ -1,5 +1,5 @@
 export class ConfigAPI {
-  //private Host = 'http://live.advikon.com/api/';
+  //private Host = 'http://live.advikon.com/api/'; 
   private Host: string = "https://api.advikon.com/api/";
   //private Host: string = "http://localhost:60328/api/";
    FillQueryCombo: string = this.Host + 'FillQueryCombo';
