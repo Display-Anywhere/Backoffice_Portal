c58988363e6ced1680f14f2e7959fa300bc0797a
Talwinder Jeet Singh <talwindergur@gmail.com>
2021-03-01 12:06:35 +0530

    changes Mar21

--- WebPanelCMS/src/app/class/ConfigAPI.ts
+++ WebPanelCMS/src/app/class/ConfigAPI.ts
@@ -1,7 +1,5 @@
 export class ConfigAPI {
   private Host = 'http://live.advikon.com/api/';
-  //private Host: string = "https://api.advikon.com/api/";
-  //private Host: string = "http://localhost:60328/api/";
    FillQueryCombo: string = this.Host + 'FillQueryCombo';
    FillTokenInfo: string = this.Host + 'FillTokenInfo';
    FillTokenContent: string = this.Host + 'FillTokenContent';
