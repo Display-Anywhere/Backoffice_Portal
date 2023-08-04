fb8fd1111cec44557f78e4241b6529231636b4b0
Talwinder Jeet Singh <>
2023-08-04 18:00:51 +0530

    host

--- WebPanelCMS/src/app/class/ConfigAPI.ts
+++ WebPanelCMS/src/app/class/ConfigAPI.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
 @Injectable()
 export class ConfigAPI {
-//  private Host = 'https://uatapi.display-anywhere.com/api/';
+  private Host = 'https://uatapi.display-anywhere.com/api/';
   //Host=localStorage.getItem('host')
   //Host: string = "https://applicationaddons.com/api/";
    //private Host: string = "https://panelapi.advikon.com/api/";
-   private Host: string = "http://localhost:60328/api/";
+   //private Host: string = "http://localhost:60328/api/";
    //private Host_panel: string = "https://api.advikon.com/api/";
    //private Host_panel: string = "http://localhost:60328/api/";
    FillQueryCombo: string = this.Host + 'FillQueryCombo';
